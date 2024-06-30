8652577c123d4d43b3c7d78bec83ed6e0ec97cef
Ritabrata Patra <teamritabrata@gmail.com>
2024-06-30 11:07:26 +0530

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page(){
+    return (
+        <p>DashBoard Page</p>
+    )
+}
+//mane folder banao..ar tar mooddhe page.tsx add koro
+//bas route created 
+//example :-- dashboard foldername...ar tarmoddhe page.tsx 
+//tahole /dashboard route created
