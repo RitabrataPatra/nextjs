7ff9439c27ac1cd856ef2bf0a0a372a08f4baacc
Ritabrata Patra <teamritabrata@gmail.com>
2024-07-01 11:27:05 +0530

    new changes

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page(){
-    return (
-        <p>DashBoard Page</p>
-    )
-}
-//mane folder banao..ar tar mooddhe page.tsx add koro
-//bas route created 
-//example :-- dashboard foldername...ar tarmoddhe page.tsx 
-//tahole /dashboard route created
